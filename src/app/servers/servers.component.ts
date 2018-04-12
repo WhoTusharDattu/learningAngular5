30c1262976ed43242ffcb71cc2f456456f4538af
WhoTusharDattu <vikytushar@gmail.com>
2018-04-12 14:48:50 +0530

    basics

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
   // template: './servers.component.html',
   templateUrl: './servers.component.html',
 
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  styles:[`
+  .white-text={
+    color:white
+  }
+  `]
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server created';
+  serverName = 'Test Server';
+  username = '';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer1'];
+  data = [];
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true;
@@ -21,7 +31,16 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
   onCreateServer() {
-    this.serverCreationStatus='server was created!';
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.data.push(new Date());
+    this.serverCreationStatus='server was created! Name is: ' + this.serverName;
+  }
+  onUpdateServerName(event : any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  onClearInput(){
+    this.username = '';
   }
 
 }
